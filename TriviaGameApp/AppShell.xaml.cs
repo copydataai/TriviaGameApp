858fa1fc7b18fc037e0c6105c3d79cb277cb075b
Jose Sanchez <G00438456@atu.ie>
2024-12-30 10:44:44 +0000

    feat: add Views

--- TriviaGameApp/AppShell.xaml.cs
+++ TriviaGameApp/AppShell.xaml.cs
@@ -1,10 +1,15 @@
-﻿namespace TriviaGameApp
+﻿using Microsoft.Maui.Controls;
+using TriviaGameApp.Views;
+namespace TriviaGameApp
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute("PlayersView", typeof(PlayersView));
+            Routing.RegisterRoute("LeaderboardPage", typeof(LeaderboardPage));
+            Routing.RegisterRoute("SettingsPage", typeof(SettingsPage));
         }
     }
 }
