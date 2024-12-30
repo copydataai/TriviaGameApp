a7b253afb86ca6614f0d9095675d8d8567ee1580
Jose Sanchez <G00438456@atu.ie>
2024-12-30 11:46:10 +0000

    chore(models): add OpenTDBResponse

---
+++ TriviaGameApp/Models/OpenTDBResponse.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace TriviaGameApp.Models
+{
+    public class OpenTDBResponse
+    {
+        [JsonPropertyName("response_code")]
+        public int ResponseCode { get; set; }
+
+        [JsonPropertyName("results")]
+        public List<OpenTDBQuestion>? Results { get; set; }
+    }
+
+    public class OpenTDBQuestion
+    {
+        [JsonPropertyName("type")]
+        public string? Type { get; set; }
+
+        [JsonPropertyName("difficulty")]
+        public string? Difficulty { get; set; }
+
+        [JsonPropertyName("category")]
+        public string? Category { get; set; }
+
+        [JsonPropertyName("question")]
+        public string? Question { get; set; }
+
+        [JsonPropertyName("correct_answer")]
+        public string? CorrectAnswer { get; set; }
+
+        [JsonPropertyName("incorrect_answers")]
+        public List<string>? IncorrectAnswers { get; set; }
+    }
+}
